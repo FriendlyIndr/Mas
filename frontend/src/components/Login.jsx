26d7c803ed3b71ad8a179507ca5ccd40c110616a
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2025-11-05 19:55:52 +0530

    Login form created.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className='flex justify-center min-h-screen items-center'>
+        <div 
+          className='text-center border rounded-lg space-y-2 w-96'
+        >
+          <div className='mt-2 grid grid-cols-2 gap-2'>
+            <label className='mr-2'>Email</label>
+
+            <label className='mr-2'>Password</label>
+            
+            <input 
+              type='email'
+              className='border'
+            />
+
+            <input 
+              type='password'
+              className='border'
+            />
+          </div>
+
+          <button
+            className='mb-2 cursor-pointer p-2 bg-blue-600 text-white rounded-lg'
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
