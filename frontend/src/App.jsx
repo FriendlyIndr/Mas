9b55f550217d87b5d5af0337fb3822d1f47733b3
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2025-11-05 17:48:45 +0530

    React router setup. Login page added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import Calendar from './components/Calendar'
+import Calendar from './components/Calendar';
+import Login from './components/Login';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Calendar />
+      <RouterProvider router={router}/>
     </>
-  )
+  );
 }
 
 export default App
