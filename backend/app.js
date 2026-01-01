d91b02e1ba75b26edd4dece99cdb693f71211cb6
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-01-01 19:50:58 +0530

    Database added

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import 'dotenv/config';
+import db from './db.js';
+
+import express from 'express';
+const app = express();
+const port = 3000;
+
+// Test db connection
+const startServer = async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1);
+  }
+}
+
+startServer();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
---
+++ backend/db.js
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize";
+
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_name = process.env.DB_NAME;
+
+const db = new Sequelize(db_name, db_user, db_pass, {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+
+export default db;
