f83f03a688949abd94e0d177f6df44648d95d41a
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-02-06 01:32:20 +0530

    db_host made into environment variable

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,10 @@ import { Sequelize } from "sequelize";
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 const db_name = process.env.DB_NAME;
+const db_host = process.env.DB_HOST;
 
 const db = new Sequelize(db_name, db_user, db_pass, {
-    host: 'localhost',
+    host: db_host,
     dialect: 'postgres',
 });
 
