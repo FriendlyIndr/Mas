e4cdd76a7de6b4c8fb1c7f3cb1387025bed98fd4
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-02-06 15:39:59 +0530

    DB setup for production

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,32 @@
 import { Sequelize } from "sequelize";
 
+const node_env = process.env.NODE_ENV;
+const isProduction = node_env === 'production';
+
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 const db_name = process.env.DB_NAME;
 const db_host = process.env.DB_HOST;
 
-const db = new Sequelize(db_name, db_user, db_pass, {
-    host: db_host,
-    dialect: 'postgres',
-});
+const db_url = process.env.DB_URL;
+
+if (isProduction && !db_url) {
+    throw new Error('DB_URL is not set in production');
+}
+
+const db = isProduction 
+    ? new Sequelize(db_url, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    })
+    : new Sequelize(db_name, db_user, db_pass, {
+        host: db_host,
+        dialect: 'postgres',
+      });
 
 export default db;
